e4ea349a4eba16f28991df24a7169091738f1ae2
hinal.chaklasiya@viprak.com <hinal.chaklasiya@viprak.com>
2023-04-03 18:19:48 +0530

    hinal

---
+++ CaptchaTry/Controllers/DefaultController.cs
@@ -0,0 +1,26 @@
+﻿using CaptchaTry.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CaptchaTry.Controllers
+{
+    public class DefaultController : Controller
+    {
+        private readonly ILogger<DefaultController> _logger;
+
+        public DefaultController(ILogger<DefaultController> logger)
+        {
+            _logger = logger;
+        }
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(LoginViewModel loginViewModel)
+        {
+            return View();
+        }
+    }
+}
---
+++ CaptchaTry/Models/LoginViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CaptchaTry.Models
+{
+    public class LoginViewModel
+    {
+        [Required(ErrorMessage = "Username Required")]
+        public string Username { get; set; }
+
+        [Required(ErrorMessage = "Password Required")]
+        public string Password { get; set; }
+    }
+}
